5bc3b10123c348a54c607f35a9792b5a94bb1080
cipher <ciphershadow1@gmail.com>
2025-12-11 14:58:22 +1100

    Fix 10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" PyMongo - update_many """
+""" PyMongo Update """
 
 import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
     """ Function changes all topics of a school doc based on the name """
-    mongo_collection.update_many({"name": name}, {$set: {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
