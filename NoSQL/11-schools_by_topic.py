9fb4d8d6e656627bfaa0eb5277715f1ec772e7e8
cipher <ciphershadow1@gmail.com>
2025-12-11 15:06:05 +1100

    Add 11-schools_by_topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" PyMongo Find """
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ Function returns list of schools based on specific topic """
+    return [i for i in mongo_collection.find({"topics": topics})]
