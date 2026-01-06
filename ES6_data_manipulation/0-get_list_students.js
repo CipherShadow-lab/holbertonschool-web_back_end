89bb84f73f0b7330c23dddc27751b46e574e4e90
CipherShadow <ciphershadow1@gmail.com>
2026-01-06 16:16:00 +1100

    add 0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+    return [
+        {
+            firstName: 'Guillaume',
+            id: 1,
+            location: 'San Fransisco',
+        },
+        {
+            firstName: 'James',
+            id: 2,
+            location: 'Columbia',
+        },
+        {
+            firstName: 'Serena',
+            id: 3,
+            location: 'San Francisco',
+        },
+    ];
+}
