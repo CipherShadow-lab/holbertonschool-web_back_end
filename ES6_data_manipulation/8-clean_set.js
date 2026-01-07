a51ca8c9db97263feb2a65ae8e1f24216b1ae695
CipherShadow <ciphershadow1@gmail.com>
2026-01-08 07:34:00 +1100

    Add verification step for undefined or empty startString

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
     const result = [];
 
+    // Check startString is not undefined or empty
     set.forEach(value => {
-        if (value.startsWith(startString)) {
+        if (startString && value.startsWith(startString)) {
             result.push(value.slice(startString.length));
+        } else if (!startString) {
+            result.push(value);
         }
     });
 
