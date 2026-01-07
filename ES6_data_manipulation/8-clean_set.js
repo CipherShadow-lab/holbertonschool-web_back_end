4108dc9b8001d09120819330edacf73bcea1c174
CipherShadow <ciphershadow1@gmail.com>
2026-01-08 07:45:51 +1100

    fixed code to return string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,18 @@
 export default function cleanSet(set, startString) {
-    const result = [];
+    let result = '';
 
-    // Check startString is not undefined or empty
-    set.forEach(value => {
-        if (startString && value.startsWith(startString)) {
-            result.push(value.slice(startString.length));
-        } else if (!startString) {
-            result.push(value);
-        }
-    });
+    // If startString is empty or undefined, include all values
+    if (!startString) {
+        set.forEach(value => {
+            result += value + '-';
+        });
+    } else {
+        set.forEach(value => {
+            if (value.startsWith(startString)) {
+                result += value.slice(startString.length) + '-';
+            }
+        });
+    }
 
-    return result.join('-');
+    return result.endsWith('-') ? result.slice(0, -1) : result;
 }
