5e1c391dac4e8c8eebae68779feeb6eea3a47fe7
CipherShadow <ciphershadow1@gmail.com>
2026-01-08 15:57:32 +1100

    Add 6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signupUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signupUser(firstName, lastName),
+        uploadPhoto(fileName)
+    ]);
+    .then(results => {
+        return results.map(result => {
+            if (result.status === 'fulfilled') {
+                return { status: 'fulfilled', value: result.value };
+            } else {
+                return { status: 'rejected', value: result.reason };
+            }
+        });
+    });
+}
