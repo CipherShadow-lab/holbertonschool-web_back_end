57c954442a5a7629da3463e8ec5f03fb570a46be
CipherShadow <ciphershadow1@gmail.com>
2026-01-13 08:37:12 +1100

    1-stdin.js Update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input:
-process.stdin.on('data', (input) => {
-  
-  // Remove newline from user input
-  const name = input.toString(),trim();
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}\n`);
+});
 
-  // Display name entered by user
-  process.stdout.write('Your name is: ${name}\n');
-
-  // Close program and display exit message
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-
-  // End process
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
