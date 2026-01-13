67ab58ed27142a43e8b8637652597da8f54a2730
CipherShadow <ciphershadow1@gmail.com>
2026-01-13 15:02:43 +1100

    Fix 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,16 +2,14 @@ const express = require('express');
 
 // Create Express app
 const app = express();
+const port = 1245;
 
 // Define endpoints for GET requests (to '/')
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Config server to listen on port 1245
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+// Config server to listen on port (1245)
+app.listen(port, () => { });
 
-// Export app
-module.export = app;
+module.exports = app;
