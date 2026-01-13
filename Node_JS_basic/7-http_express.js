6caf95e52c2fb910e2bd85bfdad5f04c818ac5ec
CipherShadow <ciphershadow1@gmail.com>
2026-01-13 15:13:37 +1100

    Add 7-http_express.js

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const LIST = args[0];
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School');
+});
+
+app.get('/students', (req, res) => {
+  const respMessage = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(LIST);
+    res.send(`${respMessage}$students.join('/n')}`);
+    } catch (error) {
+      res.send(`${respMessage}${error.message}`);
+    }
+});
+
+app.listen(port, () => { });
+
+module.exports = app;
