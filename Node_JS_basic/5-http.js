9ad7a359293e77ed49cdc84736ee1805cb910df7
CipherShadow <ciphershadow1@gmail.com>
2026-01-13 14:23:53 +1100

    Removed helper function and added import

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,46 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async.js');
 
-// Helper function that returns formatted text
-function readStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data
-      .trim()
-      .split('\n')
-      .filter((line) => line.length > 0);
-
-      const students = lines.slice(1);
-      const fields = {};
-
-      students.forEach((line) => {
-        const parts = line.split(',');
-        const firstname = parts[0];
-        const field = parts[3];
-
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      let output = `Number of students: ${students.length}\n`;
-
-      Object.keys(fields).forEach((field) => {
-        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-      });
-
-      resolve(output.trim());
-    });
-  });
-}
-
-// Create app server to handle routes (/ and /students)
+//Create app server to handle routes (/ and /students)
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -49,19 +10,19 @@ const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
-    const database = process.argv[2];
-
-
-    readStudents(database)
-      .then((data) => {
-        res.end(data);
+    countStudents(process.argv[2])
+      .then((content) => {
+        res.end(content);
       })
       .catch(() => {
-        res.end('Cannot load the database\n');
+        res.end(Cannot load the database\n');
       });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
 
-app.listen(1245);
-
 module.exports = app;
+
+app.listen(1245);
