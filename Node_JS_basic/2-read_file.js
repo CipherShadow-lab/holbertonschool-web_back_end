734e664d8405d21b26df0a91bf0ad7092d496c80
CipherShadow <ciphershadow1@gmail.com>
2026-01-13 12:12:43 +1100

    Fixed: indentations

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -15,30 +15,30 @@ function countStudents(path) {
     .split('\n')
     .filter((line) => line.length > 0);
 
-    // Remove header row
-    const students = lines.slice(1);
+  // Remove header row
+  const students = lines.slice(1);
 
-    // Count total students
-    console.log(`Number of students: ${students.length}`);
+  // Count total students
+  console.log(`Number of students: ${students.length}`);
 
-    // Group students by field
-    const fields = {};
+  // Group students by field
+  const fields = {};
 
-    students.forEach((line) => {
-      const [firstname, , , field] = line.split(',');
+  students.forEach((line) => {
+    const [firstname, , , field] = line.split(',');
 
-      if(!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstname);
-    });
+    if(!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(firstname);
+  });
 
-    // Print each field's summary
-    Object.keys(fields).forEach((field) => {
-      const list = fields[field].join(', ');
-      console.log(
-        `Number of students in ${field}: ${fields[field].length}. List: ${list}`
-      );
+  // Print each field's summary
+  Object.keys(fields).forEach((field) => {
+    const list = fields[field].join(', ');
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${list}`
+    );
   });
 }
 
