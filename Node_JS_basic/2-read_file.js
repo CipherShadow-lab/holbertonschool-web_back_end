f9529ef07118d5f04752b4f23f17c6fa03390e78
CipherShadow <ciphershadow1@gmail.com>
2026-01-13 11:58:10 +1100

    Add 2-read_file.js

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let data;
+
+  try {
+    data = fs.readFileSync(path, 'utf8);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  // Split into lines and remove empty lines
+  const lines = data
+    .trim()
+    .split('\n')
+    .filter((line) => line.length > 0);
+
+    // Remove header row
+    const students = line.slice(1);
+
+    // Count total students
+    console.log(`Number of students: ${students.length}`);
+
+    // Group students by field
+    const fields = {};
+
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if(!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    // Print each field's summary
+    Object.keys(fields).forEach((field) => {
+      const list = fields[field].join(', ');
+      console.log(
+        `Number of students in ${field}: ${fields[field].length}. List: ${list}`
+    };
+  });
+}
+
+module.exports = countStudents;
